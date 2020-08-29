adaccb9d38bb48e1888e47c9ab895e9a57778ddf
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-29 11:32:36 -0300

    add react-router-dom config routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route path='/' exact component={StreamList} />
+        <Route path='/streams/new' component={StreamCreate} />
+        <Route path='/streams/edit' component={StreamEdit} />
+        <Route path='/streams/delete' component={StreamDelete} />
+        <Route path='/streams/show' component={StreamShow} />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
