1af9a1658031c452706c0be80234992287bf0ffd
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-08-30 18:07:44 -0300

    add login with google using OAuth2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route        } from 'react-router-dom';
 import StreamCreate from './components/streams/StreamCreate';
 import StreamDelete from './components/streams/StreamDelete';
 import StreamEdit from './components/streams/StreamEdit';
 import StreamList from './components/streams/StreamList';
 import StreamShow from './components/streams/StreamShow';
+import Header from './components/Header'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path='/' exact component={StreamList} />
-        <Route path='/streams/new' component={StreamCreate} />
-        <Route path='/streams/edit' component={StreamEdit} />
-        <Route path='/streams/delete' component={StreamDelete} />
-        <Route path='/streams/show' component={StreamShow} />
-      </div>
-    </BrowserRouter>
+    <div className='ui container'>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamCreate} />
+          <Route path='/streams/edit' component={StreamEdit} />
+          <Route path='/streams/delete' component={StreamDelete} />
+          <Route path='/streams/show' component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
